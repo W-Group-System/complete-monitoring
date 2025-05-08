ec5c31324cf6e1600a0b108d5c41900aa9d4b0cd
JunJihad <junjihadbarroga@gmail.com>
2025-05-08 12:01:24 +0800

    loader

--- resources/views/login-token-redirect.blade.php
+++ resources/views/login-token-redirect.blade.php
@@ -1,10 +1,22 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Redirecting...</title>
+    <style>
+        .loader {
+                position: fixed;
+                left: 0px;
+                top: 0px;
+                width: 100%;
+                height: 100%;
+                z-index: 9999;
+                background: url("{{ asset('/img/3.gif')}}") 50% 50% no-repeat rgb(249,249,249) ;
+                opacity: .8;
+                background-size:200px 120px;
+            }
+    </style>
 </head>
 <body>
-    <p>Logging you in...</p>
+    <div class="loader"></div>
     <script>
         const token = sessionStorage.getItem('api_token');
         if (token) {
